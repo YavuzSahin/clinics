cf842aab371ef07541d85df9827e66ad967a2fde
yavuzsahin <yavuz.sahins@gmail.com>
2020-11-09 19:17:24 +0300

    İşletme dış linkleri güncellendi.

---
+++ outlink.php
@@ -0,0 +1,6 @@
+<?php require_once 'template/header.php';?>
+<div class="container text-center">
+    Site dışında bir bağlantıya yönlendiriliyorsunuz!<br>
+    Lütfen bekleyiniz..
+</div>
+<?php require_once 'template/footer.php';?>
