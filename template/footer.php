2a32132e59c7e94f1e7b6472d1aad4e10048558e
yavuzsahin <yavuz.sahins@gmail.com>
2020-11-10 23:15:51 +0300

    İşletme Profili eklendi

--- template/footer.php
+++ template/footer.php
@@ -20,7 +20,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.sticky/1.0.4/jquery.sticky.min.js" integrity="sha512-QABeEm/oYtKZVyaO8mQQjePTPplrV8qoT7PrwHDJCBLqZl5UmuPi3APEcWwtTNOiH24psax69XPQtEo5dAkGcA==" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function(){
-        $(".tr-sticky").sticky({topSpacing:0});
+        $(".theiaStickySidebar").sticky({topSpacing:0});
     });
 </script>
 </body>
