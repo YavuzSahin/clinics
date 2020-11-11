5388f515bd00c0eb5fbfee6408e9d65a114efd28
yavuzsahin <yavuz.sahins@gmail.com>
2020-11-11 20:50:37 +0300

    Otomatik Klinik Ekleme Ekleniyor.

--- admin/klinikekle.php
+++ admin/klinikekle.php
@@ -144,8 +144,7 @@ $site = $db->table('site')->where('id', 1)->get();
 
                                     $html = file_get_html('https://www.sacekimiburada.com/'.$url.'-sac-ekim-merkezleri');
                                     foreach($html->find('#sideItemRf .pr-item') as $element){
-                                        echo $element->find('h3');
-                                        echo $element->find('img')->src;
+                                        print_r($element->find('h3'));
                                         echo "<br><hr>";
                                     }
                                 }
