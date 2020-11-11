398f7a7ad76ab95c130b554f659301841b1abd01
yavuzsahin <yavuz.sahins@gmail.com>
2020-11-11 03:31:57 +0300

    İşletme Profili eklendi

--- template/footer.php
+++ template/footer.php
@@ -26,7 +26,7 @@
         }
         $('.slick-content').slick({
             dots: true,
-            infinite: false,
+            infinite: true,
             speed: 300,
             slidesToShow: 4,
             slidesToScroll: 4,
@@ -54,9 +54,6 @@
                         slidesToScroll: 1
                     }
                 }
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
             ]
         });
     });
