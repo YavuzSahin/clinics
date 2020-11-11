65a04bfbd9e4b1786fb6d8825a4c82965fe2d0f1
yavuzsahin <yavuz.sahins@gmail.com>
2020-11-11 03:58:38 +0300

    İşletme Profili eklendi

--- template/footer.php
+++ template/footer.php
@@ -31,7 +31,7 @@
             speed: 300,
             autoplay:true,
             autoplaySpeed:5000,
-            slidesToShow: 1,
+            slidesToShow: 2,
             slidesToScroll: 1,
         });
     });
