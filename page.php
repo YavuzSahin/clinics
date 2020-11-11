623913e501e2b1fcc79a290a0fad0e80d1b4641b
yavuzsahin <yavuz.sahins@gmail.com>
2020-11-11 16:58:12 +0300

    Sayfa gösterimi aktif edildi.

--- page.php
+++ page.php
@@ -2,17 +2,17 @@
 ?>
 <div class="container clinics">
     <div class="page-breadcrumbs">
-        <h1 class="section-title"><?=$siteBaslikic;?></h1>
+        <h1 class="section-title"><?=$pageInfo->baslik;?></h1>
         <div class="world-nav cat-menu">
             <ul class="list-inline">
-                <li class="active"><a href="<?=$actual_link;?>"><?=$siteBaslikic;?></a></li>
+                <li class="active"><a href="<?=$actual_link;?>"><?=$pageInfo->baslik;?></a></li>
             </ul>
         </div>
     </div>
     <div class="col-md-4 col-lg-3 float-left tr-sticky">
         <div id="sitebar" class="theiaStickySidebar">
                 <div class="widget">
-                    <h2 class="section-title title"><?=$siteBaslikic;?></h2>
+                    <h2 class="section-title title"><?=$site->baslik_ic;?></h2>
                     <h3>Saç Ekimi Bilgilendirme</h3>
                     <ul class="post-list">
                         <?php
@@ -33,6 +33,7 @@
                             </li>
                         <?php } ?>
                     </ul>
+                    <h3>Saç Ekimi Kategorileri</h3>
                     <ul class="post-list">
                         <?php
                         $categories = $db->table('kategori')->orderBy('id', 'ASC')->getAll();
@@ -60,54 +61,9 @@
             <div class="row">
                 <div class="col">
                     <div class="left-content details-page">
-                        <h3><span><?=$siteBaslikic;?></span> Hakkında</h3>
+                        <h3><span><?=$pageInfo->baslik;?></span></h3>
                         <div class="clinic-detail">
-                            <?=$pageInfo->hakkinda;?>
-                            <p><strong><?=sehir($pageInfo->city);?></strong> saç ekim merkezleri arasında faaliyet gösteren <strong><?=$siteBaslikic;?></strong>, FUE Saç Ekimi ve DHI Saç Ekimi gibi saç ekimi çözümlerini saç ekimi hastalarına özel alternatif sunarak saç ekimi operasyonlarını gerçekleştirmektedir.<br>
-                                Saç ekimi alanında, mükemmel saç ekimi sonucu başarılara imza atan <strong><?=$siteBaslikic;?></strong>, uzman kadrosu ile ihtiyaçları analiz ederek, en hızlı, uygun ve kaliteli hizmet politikası ile saç ekimi hastalarına hizmet vermektedir.<br>
-                                Uzun yıllara dayanan tecrübesiyle, saç ekimi teknolojilerini ve gelişmelerini yakından takip ederek, hastalarına en uygun saç ekimi yöntemlerini sunmayı hedeflemektedir.<br>
-                                Yurtiçinden ve yurtdışından geniş bir kitleye hitap eden <strong><?=$siteBaslikic;?></strong> günden güne saç ekimi başarısını arttırmaktadır.<br>
-                                Türkiye’de tüm şehirlerimizden, yurtdışından ise genellikle Avrupadan; İngiltere, Almanya, Hollanda, Belçika, Fransa, İrlanda, İsveç ve İsviçreden yoğunlukta gelmekteyken, Arap ülkelerinden; Suudi Arabistan, Dubai (Birleşik Arap Emirlikleri), Katar, Kuveyt, Libya, Mısır ve Irak gibi başlıca ülkelerden sıkça gelmektedir.<br>
-                                Saç ekimi yaptıran hastaların memnuniyet oranı bir hayli yüksek olan <strong><?=$siteBaslikic;?></strong> saç ekimi işlemi sonrasında da hastalarına gereken tüm hizmetleri vermektedir.<br>
-                                <strong>Saç ekimi <?=sehir($pageInfo->city);?></strong> denince akla ilk gelen merkezlerden biri olan <strong><?=$siteBaslikic;?></strong> saç ekimi konusundaki çalışmalarını özenle devam ettirmektedir.<br>
-                                <a href="<?=$site->url;?>" title="saç ekimi merkezleri">Saç ekimi merkezleri</a> hakkında bilgi almak istiyorsanız, <?=$site->baslik_ic;?> sitemizi takip etmeye devam edebilirsiniz..</p>
-                        </div>
-                        <h3><span><?=$siteBaslikic;?></span> Yorumları</h3>
-                        <div class="clinic-detail-slider">
-                            <div class="slick-content yorum">
-                                <?php
-                                    $comments = $db->table('yorum')->where('merkezid', $pageInfo->id)->where('onay', 1)->where('type', 1)->getAll();
-                                    foreach ($comments as $comment){
-                                ?>
-                                <div class="item">
-                                    <div class="comments">
-                                        <p><strong><i class="fa fa-user"></i> <?=$comment->isim;?></strong> demiş ki;</p>
-                                        <p class="comment-inside"><?=$comment->yorum;?></p>
-                                        <small><i class="fa fa-clock-o"></i> <?php echo iconv('latin5','utf-8',strftime(' %d %B %Y, %A ',strtotime($comment->eklenme_tarihi)));?></small>
-                                    </div>
-                                </div>
-                                <?php } ?>
-                            </div>
-                        </div>
-                        <h3><span><?=$siteBaslikic;?></span> Şikayetleri</h3>
-                        <div class="clinic-detail-slider">
-                            <div class="slick-content sikayetn">
-                                <?php
-                                $comments = $db->table('yorum')->where('merkezid', $pageInfo->id)->where('onay', 1)->where('type', 2)->getAll();
-                                if(count((array) $comments)>=1){
-                                foreach ($comments as $comment){
-                                    ?>
-                                    <div class="item">
-                                        <div class="comments">
-                                            <p><strong><i class="fa fa-user"></i> <?=$comment->isim;?></strong> demiş ki;</p>
-                                            <p class="comment-inside"><?=$comment->yorum;?></p>
-                                            <small><i class="fa fa-clock-o"></i> <?php echo iconv('latin5','utf-8',strftime(' %d %B %Y, %A ',strtotime($comment->eklenme_tarihi)));?></small>
-                                        </div>
-                                    </div>
-                                <?php } }else{ ?>
-                                    <div class="alert alert-success col-md-12"><?=$siteBaslikic;?> hakkında hiç şikayet bulunamadı!</div>
-                                <?php } ?>
-                            </div>
+                            <?=$pageInfo->icerik;?>
                         </div>
                     </div>
                 </div>
